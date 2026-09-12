ac4678dd3b161b840ec249541e9398b850614a08
khatermohamed615-sudo <khatermohamed615@gmail.com>
2026-09-12 12:36:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,9 +6,9 @@ const SHELL_FILES = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
 ];
 
 self.addEventListener("install", (event) => {
